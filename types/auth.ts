deff1a457698a829753d9c0cc7556de37dca7974
unknown <mutheedavid12@gmail.com>
2026-04-06 15:03:33 +0300

    refactor(authentication-types): rafcators the login/register types to use zod for form validation

--- types/auth.ts
+++ types/auth.ts
@@ -1,13 +1,40 @@
-export types FormDate = {
-    name: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-}
+import {
+  UseFormRegister,
+  FieldError,
+  FieldValues,
+  Path,
+} from "react-hook-form";
+import { z } from "zod";
 
-export type FormFieldProps = {
-    type: string;
-    placeholder: string;
-    name: string;
-    register: UseFormRegister
-}
+export const UserSchema = z
+  .object({
+    name: z.string().min(2, "Name must be at least 2 characters long"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export type FormData = z.infer<typeof UserSchema>;
+
+export const LoginSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+});
+
+export type LoginFormData = z.infer<typeof LoginSchema>;
+
+export type FormFieldProps<TFieldValues extends FieldValues> = {
+  type: string;
+  placeholder: string;
+  name: Path<TFieldValues>;
+  register: UseFormRegister<TFieldValues>;
+  error: FieldError | undefined;
+  valueAsNumber?: boolean;
+  className?: string;
+};
+
+export type ValidFieldNames = "name" | "email" | "password" | "confirmPassword";

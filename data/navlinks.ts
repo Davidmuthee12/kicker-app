5dff18a4bc4cbb2439151e40dd287c72c2a9ac5f
unknown <mutheedavid12@gmail.com>
2026-04-11 16:58:59 +0300

    feat: adds navlink data for dynamic display in navbar

---
+++ data/navlinks.ts
@@ -0,0 +1,7 @@
+export const navlinks = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Contact", path: "/contact" },
+  { name: "Pricing", path: "/pricing" },
+  { name: "Workout", path: "/workout" },
+];

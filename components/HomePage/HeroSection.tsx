1924cf3ca4cc3f9ac74be0440e1803a0429910a4
unknown <mutheedavid12@gmail.com>
2026-04-11 17:00:20 +0300

    feat: setup the hero section

--- components/HomePage/HeroSection.tsx
+++ components/HomePage/HeroSection.tsx
@@ -1,7 +1,37 @@
+import Link from "next/link";
+import { BadgeCheck } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+
 export default function HeroSection() {
   return (
     <>
-      <h1>This is the official hero section</h1>
+      <div
+        className="relative grid min-h-[100vh] grid-cols-2 bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: "url('/hero.png')" }}
+      >
+        <div className="absolute inset-0 bg-black/45" />
+
+        <div className="relative z-10 flex flex-col items-start justify-center gap-10 p-10">
+          <Badge variant="secondary">
+            <BadgeCheck data-icon="inline-start" />
+            Verified
+          </Badge>
+          <h1 className="text-white">
+            BE STRONGER THAN <br /> YOUR EXCUSES
+          </h1>
+          <p className="text-white">
+            Join us to achieve your fitness goals and transform your life.
+          </p>
+          <Link
+            href="/auth/register"
+            className="bg-blue-400 text-white p-3 rounded-sm"
+          >
+            Sign Up
+          </Link>
+        </div>
+
+        <div className="relative z-10">right</div>
+      </div>
     </>
   );
 }

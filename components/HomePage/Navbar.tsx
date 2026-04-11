12a1970f69957dc927527e1c075bec20f1f1e22b
unknown <mutheedavid12@gmail.com>
2026-04-11 16:59:54 +0300

    revamp: dynamically add navlinks from data folder

--- components/HomePage/Navbar.tsx
+++ components/HomePage/Navbar.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
+import { navlinks } from "@/data/navlinks";
 
 export default function Navbar() {
   return (
     <>
-      <div className="flex items-center justify-between p-5 bg-blue-400">
+      <div className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between bg-blue-400 p-5">
         <div>
           <h1>Kicker App</h1>
         </div>
         <div className="flex gap-5">
-          <Link href="#">About Us</Link>
-          <Link href="#">Pricing</Link>
-          <Link href="#">Services</Link>
-          <Link href="#">Schedule</Link>
+          {navlinks.map((link) => (
+            <Link key={link.name} href={link.path}>
+              {link.name}
+            </Link>
+          ))}
         </div>
 
         <div className="flex bg-white text-gray-600 p-2 rounded-sm">

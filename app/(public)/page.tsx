957f895d838ba0498a875c9a739dc03027518d9e
unknown <mutheedavid12@gmail.com>
2026-04-12 14:18:50 +0300

    feat: adds new section tolanding page

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,11 +1,17 @@
+import CallAction from "@/components/HomePage/CallAction";
 import HeroSection from "@/components/HomePage/HeroSection";
+import JoinSection from "@/components/HomePage/JoinSection";
 import Navbar from "@/components/HomePage/Navbar";
+import Services from "@/components/HomePage/Service";
 
 export default function Home() {
   return (
     <div className="pt-20">
       <Navbar />
       <HeroSection />
+      <CallAction />
+      <Services />
+      <JoinSection />
     </div>
   );
 }
